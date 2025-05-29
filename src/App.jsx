ecc933add5ab45056147bd877d92b75d04feb1a9
Anuj Pal <palanuj247@gmail.com>
2025-05-29 10:21:52 +0530

    configured daisyUI with night theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <button className="btn btn-soft btn-accent">Accent</button>;
 }
 
 export default App;
